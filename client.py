d8eb46ec59e1602d816ebc01b4f9a61612a70188
Karthick Sriram <karthicksriram71@gmail.com>
2023-05-18 22:11:08 +0530

    added room chat

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-from inputimeout import inputimeout
 import select
 import sys
 
@@ -8,8 +7,51 @@ c = socket.socket()
 
 c.connect(('localhost',9999))
 name=input("Enter your name: ")
-c.send(bytes(name,'utf8'))
-print(c.recv(1024).decode())
+c.send(bytes(name,'utf-8'))
+
+def create():
+    
+    roomName=input("Enter the chat room name to create:")
+    c.send(bytes(str(roomName),'utf8'))
+    print(c.recv(1024).decode())
+    
+
+def join():
+    i=c.recv(1024).decode()
+
+    if int(i)==1:
+        print(c.recv(1024).decode())
+        room()
+    else:
+        print("Room Number \t:\tRoom Number\t")
+        rooms=list(map(str,(c.recv(1024).decode()).split("-")))
+        for i in range(len(rooms)): 
+                print(f"{i+1}\t:\t{rooms[i]}")
+        r=int(input("Enter the room number: "))
+        if r-1<=len(rooms):
+            c.send(bytes(str(r),'utf-8'))
+        else:
+            c.send(bytes("1111","utf-8"))
+            print("Wrong input! Try again")
+            join()
+
+ 
+def room():
+    print("1. create new chat room\n2. join existing chatroom\n3. show existing chatroom")
+    ui=int(input("Enter an option: "))
+    c.send(bytes(str(ui),'utf-8'))
+    if ui==3:
+        for i in list(map(str,c.recv(1024).decode().split())):
+            print(i,end="\n")
+        room()
+    elif ui==2:
+        join()
+    elif ui==1:
+        create()
+
+    
+
+
 
 def recieve():
     msg=c.recv(1024).decode()
@@ -17,18 +59,14 @@ def recieve():
 
 def sendmsg(name):
 
-        #read_sockets,write_socket, error_socket = select.select(sys.stdin,[],[])
-
-
         msg = sys.stdin.readline()
-        print("--------------------")
         if msg!='':
             msg1=f'\n{name}>> '+str(msg)
             c.send(bytes(msg1,'utf-8'))
 
 
 
-
+room()
 
 
 while True:
--- server.py
+++ server.py
@@ -8,9 +8,9 @@ s.bind(('localhost',9999))
 s.listen(100)
 print('Waiting for connections')
 
-usr=[]
+usr={}
 
-def client(c, addr,name):
+def client(name,rn):
  
     while True:
             try:
@@ -18,32 +18,90 @@ def client(c, addr,name):
                 if message:
                      print ('\n' + message)
                      msg= message
-                     msg_send(msg, c)
+                     msg_send(msg,c,rn)
                 else:
                     remove(c)
             except:
                 continue
-def msg_send(msg, c):
-    for i in usr:
-        # if i!=c:
+
+def msg_send(msg, c,roomName):
+    print(usr)
+    for i in usr.keys():
+        if usr[i] == roomName:
             try:
                 i.send(bytes(msg, 'utf-8'))
-            except:
+            except Exception:
                 i.close()
                 remove(i)
+            else:
+                continue
 
 def remove(c):
     if c  in usr:
         usr.remove(c)
 
+rooms=[]
+def create_room(name):
+    roomName=c.recv(1024).decode()
+    rooms.append(roomName)
+    usr[c]=roomName
+    c.send(bytes("Welcome to the chat room",'utf-8'))
+    _thread.start_new_thread(client,(name,roomName)) 
+
+def join_room(name):
+    print("In join function")
+    if len(rooms)==0:
+        c.send(bytes("1",'utf-8'))
+        c.send(bytes("No Rooms Found \n create a new room",'utf-8'))
+        room(name)
+    else:
+        c.send(bytes("0",'utf-8'))
+        roomList="-".join(rooms)
+        c.send(bytes(str(rooms),"utf-8"))
+        r=int(c.recv(1024).decode())
+        if r==1111:
+            print("wrong room")
+            join_room(name)
+        try:
+            usr[c]=rooms[r-1]
+            c.send(bytes("Welcome to the chat room",'utf-8'))
+            _thread.start_new_thread(client,(name,roomName))
+        except:
+            pass
+        
+        
+        
+        
+
+
+   
+def show_room(name):
+    if len(rooms) != 0:
+        roomList=" ".join(rooms)
+        c.send(bytes(roomList,"utf-8"))
+    room(name)
+
+
+def room(name):
+
+    ui=int(c.recv(1024).decode())
+    if ui==1:
+        
+        create_room(name)
+    elif ui==2:
+        join_room(name)
+    else:
+        show_room(name)
+
 while True:
 
     c, addr = s.accept()
     name=c.recv(1024).decode()
-    usr.append(c)
-    print("Welcome to the chat",name) 
-    c.send(bytes("Welcome to the chat!!",'utf-8'))
-    _thread.start_new_thread(client,(c,addr,name))
+    usr[c]=""
+    print("connections accepted")
+    room(name)
+
+    
 c.close()
 s.close()
 
